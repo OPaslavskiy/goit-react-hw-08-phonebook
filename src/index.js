09e7ad7c9a43019b23d5bd92e41ef5e686b9b7bf
OPaslavskiy <bankir.op@gmail.com>
2023-04-15 16:45:05 +0300

    ашч

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { store } from './redux/store';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
